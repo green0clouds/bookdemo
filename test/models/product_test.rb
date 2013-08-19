3e757b2902041e57749c53b330b3e6fac3b949a0
green0clouds <pishchulin@gmail.com>
2013-08-19 16:33:06 +0000

    Validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   fixtures :products
+   test "product attributes must not be empty" do
+      product = Product.new
+      assert product.invalid?
+      assert product.errors[:title].any?
+      assert product.errors[:description].any?
+      assert product.errors[:price].any?
+  end
+
+   test "second test" do
+      product = Product.new(title: "my book", description: "yyy", image_url: "image_url.jpg")
+      product.price = -1
+      assert product.invalid?
+      assert_equal "must be greater than or equal to 0.01", product.errors[:price].join(';')
+      product.price = 0
+      assert product.invalid?
+      assert_equal "must be greater than or equal to 0.01", product.errors[:price].join(';')
+      product.price = 1
+      assert product.valid?
+   end
+ 
+   test "third test" do
+      product = Product.new(title: products(:ruby).title, description: "descr", price: 1, image_url: "test")
+      assert !product.save
+      assert_equal "has already been taken", product.errors[:title].join('; ')
+      
+   end
+   
+
+
 end
